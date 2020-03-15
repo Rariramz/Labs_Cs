34f8bfc29e32dcfc78435635a0092a0c1ee1c8a2
Rariramz <61017873+Rariramz@users.noreply.github.com>
2020-03-15 20:40:18 +0300

    Update Program.cs
    ввод/вывод матриц теперь в program :)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,9 +11,11 @@ namespace lab1_Csharp
             while (true)
             {
                 int ans = 0;
+                MatrixFromConsole consoleMatrix1 = new MatrixFromConsole();
+                MatrixFromConsole consoleMatrix2 = new MatrixFromConsole();
                 Matrix matrix1, matrix2, matrix3;
 
-                Matrix.ShowMenuOfMatrixOperations();            //Выбор опции
+                MatrixFromConsole.ShowMenuOfMatrixOperations();            //Выбор опции
                 while (ans == 0)
                 {
                     try
@@ -23,9 +25,7 @@ namespace lab1_Csharp
                         ans = int.Parse(Console.ReadLine());
                         Console.ForegroundColor = ConsoleColor.White;
                         if (!(ans >= 1 && ans <= 6))
-                        {
                             throw new Exception();
-                        }
                     }
                     catch (Exception)
                     {
@@ -33,55 +33,123 @@ namespace lab1_Csharp
                         Console.WriteLine("Wrong input! Try again, please :)");
                     }
                 }
-                switch (ans)
-                {
-                    case 1:
-                        {
-                            matrix1 = new Matrix(true);
-                            matrix1.FillMatrix();
-                            matrix2 = new Matrix(true);
-                            matrix2.FillMatrix();
-                            matrix3 = matrix1 + matrix2;
-                            break;
-                        }
-                    case 2:
-                        {
-                            matrix1 = new Matrix(true);
-                            matrix1.FillMatrix();
-                            matrix2 = new Matrix(true);
-                            matrix2.FillMatrix();
-                            matrix3 = matrix1 - matrix2;
-                            break;
-                        }
-                    case 3:
-                        {
-                            matrix1 = new Matrix(true);
-                            matrix1.FillMatrix();
-                            matrix3 = Matrix.MatrixMultiplicationByNumber(matrix1);
-                            break;
-                        }
-                    case 4:
-                        {
-                            matrix1 = new Matrix(true);
-                            matrix1.FillMatrix();
-                            matrix2 = new Matrix(true);
-                            matrix2.FillMatrix();
-                            matrix3 = matrix1 * matrix2;
-                            break;
-                        }
-                    case 5:
-                        {
-                            matrix1 = new Matrix(true);
-                            matrix1.FillMatrix();
-                            matrix3 = Matrix.GetTransposeMatrix(matrix1);
-                            break;
-                        }
-                    case 6:
-                        Environment.Exit(0);
-                        break;
-                }
-            }
 
+                if (ans != 3 && ans != 5 && ans != 6)        //операции с двумя матрицами
+                {
+                    consoleMatrix1.SetSize();
+                    consoleMatrix1.FillMatrix();
+                    consoleMatrix2.SetSize();
+                    consoleMatrix2.FillMatrix();
+                    matrix1 = new Matrix(consoleMatrix1.GetMatrixFromConsole());
+                    matrix2 = new Matrix(consoleMatrix2.GetMatrixFromConsole());
+
+                    switch (ans)
+                    {
+                        case 1:
+                            try
+                            {
+                                matrix3 = matrix1 + matrix2;
+                                Console.WriteLine("The result of adding matrices");
+                                MatrixFromConsole.PrintMatrix(matrix1);
+                                Console.WriteLine("   and matrix");
+                                MatrixFromConsole.PrintMatrix(matrix2);
+                                Console.WriteLine("     is matrix");
+                                MatrixFromConsole.PrintMatrix(matrix3);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("\nThese matrices cannot be added :(");
+                            }
+                            break;
+                        case 2:
+                            try
+                            {
+                                matrix3 = matrix1 - matrix2;
+                                Console.WriteLine("The result of substracting from matrix");
+                                MatrixFromConsole.PrintMatrix(matrix1);
+                                Console.WriteLine("   of matrix");
+                                MatrixFromConsole.PrintMatrix(matrix2);
+                                Console.WriteLine("     is matrix");
+                                MatrixFromConsole.PrintMatrix(matrix3);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("\nThese matrices cannot be substract :(");
+                            }
+                            break;
+                        case 4:
+                            try
+                            {
+                                matrix3 = matrix1 * matrix2;
+                                Console.WriteLine("The result of multiplying matrix");
+                                MatrixFromConsole.PrintMatrix(matrix1);
+                                Console.WriteLine("   and matrix");
+                                MatrixFromConsole.PrintMatrix(matrix2);
+                                Console.WriteLine("     is matrix");
+                                MatrixFromConsole.PrintMatrix(matrix3);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("\nThese matrices cannot be multiplied :(");
+                            }
+                            break;
+                    }
+                }
+                else if (ans != 6)                          //операции с одной матрицей
+                {
+                    consoleMatrix1.SetSize();
+                    consoleMatrix1.FillMatrix();
+                    matrix1 = new Matrix(consoleMatrix1.GetMatrixFromConsole());
+
+                    switch (ans)
+                    {
+                        case 3:
+                            {
+                                int k = 0;
+                                bool tmp = false;
+
+                                do
+                                {
+                                    try
+                                    {
+                                        Console.Write("\nEnter the number by which you want to multiply the matrix\nk = ");
+                                        k = int.Parse(Console.ReadLine());
+                                        tmp = true;
+                                    }
+                                    catch (Exception)
+                                    {
+                                        tmp = false;
+                                        Console.WriteLine("Wrong input! Try again, please :)");
+                                    }
+                                } while (!tmp);
+
+                                matrix3 = Matrix.MatrixMultiplicationByNumber(matrix1, k); Console.Clear();
+                                Console.WriteLine("The result of multiplying matrix");
+                                MatrixFromConsole.PrintMatrix(matrix1);
+                                Console.WriteLine("   by a nummber {0}", k);
+                                Console.WriteLine("     is matrix");
+                                MatrixFromConsole.PrintMatrix(matrix3);
+                                break;
+                            }
+                        case 5:
+                            try
+                            {
+                                matrix3 = Matrix.GetTransposeMatrix(matrix1);
+                                Console.WriteLine("The result of transposing matrix");
+                                MatrixFromConsole.PrintMatrix(matrix1);
+                                Console.WriteLine("     is matrix");
+                                MatrixFromConsole.PrintMatrix(matrix3);
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("This function is available only for square matrices :(");
+                            }
+                            break;
+                    }
+                }
+                else
+                    Environment.Exit(0);
+            }
         }
     }
 }
